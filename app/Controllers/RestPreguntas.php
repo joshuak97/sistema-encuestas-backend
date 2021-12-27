b424e363520d4a8942bd22764a93d31bdb996d2a
joshuak50 <cruzjoshuak@gmail.com>
2021-12-26 18:19:10 -0600

    se guardan cambios

--- app/Controllers/RestPreguntas.php
+++ app/Controllers/RestPreguntas.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use CodeIgniter\RESTful\ResourceController;
+use App\Models\RespuestaModel;
+use App\Models\PreguntaModel;
 
 class RestPreguntas extends ResourceController
 {
@@ -11,11 +13,29 @@ class RestPreguntas extends ResourceController
 
  public function index()
  {
-  return $this->respond($this->model->findAll());
+  $results = $this->model->findAll();
+  if (!empty($results)) {
+    return $this->respond(array(
+        "status" => 200,
+        "total_results" => count($results),
+        "message" => $results     
+        ));
+  } else {
+    return $this->respond(array(
+        "status" => 404,
+        "total_results" => 0,
+        "message" => "No se encontraron resultados"     
+        ));    
+  }
+ }
+
+ public function create()
+ {
+  $cuestionario = new CuestionarioModel();   
+  $pregunta = new PreguntaModel();
+
+  
+  
  }
  
- private function genericResponse($data,$msj,$code)
- {
-    
- }
 }
